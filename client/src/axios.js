5cae1e6c0eb1e095c8e05f652a61e4caf02804f5
a-kesss <azamkess@gmail.com>
2024-12-04 09:37:24 +0300

    Start Real

--- client/src/axios.js
+++ client/src/axios.js
@@ -1,20 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseUrl: import.meta.env.VITE_URL,
-});
-
-let accessToken = '';
-
-export function setAccessToken(newToken) {
-  accessToken = newToken;
-}
-
-axiosInstance.interceptors.request.use((config) => {
-  if (!config.headers.Authorization) {
-    config.headers.Authorization = `Bearer ${accessToken}`;
-  }
-  return config;
+  baseURl: import.meta.env.VITE_URL,
 });
 
 export default axiosInstance;
